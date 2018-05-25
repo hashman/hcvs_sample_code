8e0df729d9d684e403166ca4d514e6c5ceac10b8
YiXiang <s9801077@gmail.com>
2018-05-25 21:50:58 +0800

    Add test get sample code

---
+++ test_get.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+<h2>輸入的資料：</h2>
+<label>Search： <?=$_GET['search'] ?? '' ?></label><br>
+<label>Limit： <?=$_GET['limit'] ?? '' ?></label><br>
+<label>Type： <?=$_GET['type'] ?? '' ?></label><br>
+</body>
+</html>
