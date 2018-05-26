25906f72d5528522b5dcdf6c984c32ed27f53319
YiXiang <s9801077@gmail.com>
2018-05-26 13:42:05 +0800

    Add sample sql

--- sample.sql
+++ sample.sql
@@ -8,10 +8,19 @@ create table counter (
 
 INSERT INTO count (account, count) VALUES ('hash', 0);
 
+INSERT INTO users (account, password) VALUES ('admin', 123456);
+
+-- Create TABLE SQL
+
 CREATE TABLE `users` (
   `id` int UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   `account` varchar(100) UNIQUE NOT NULL,
-  `password` varchar(100) NOT NULL
+  `password` varchar(100) NOT NULL,
+  `points` int(11) DEFAULT 0
 );
 
-INSERT INTO users (account, password) VALUES ('admin', 123456);
+CREATE TABLE `products` (
+  `id` int UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  `name` varchar(100) UNIQUE NOT NULL,
+  `price` int(11) DEFAULT 0
+);
